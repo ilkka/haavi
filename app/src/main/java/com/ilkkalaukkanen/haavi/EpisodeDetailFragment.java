9f0f253a6d14fab93fbaff22613e2ec851f2ba60
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2014-01-11 22:30:38 +0200

    Initial commit

---
+++ app/src/main/java/com/ilkkalaukkanen/haavi/EpisodeDetailFragment.java
@@ -0,0 +1,61 @@
+package com.ilkkalaukkanen.haavi;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ilkkalaukkanen.haavi.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Episode detail screen.
+ * This fragment is either contained in a {@link EpisodeListActivity}
+ * in two-pane mode (on tablets) or a {@link EpisodeDetailActivity}
+ * on handsets.
+ */
+public class EpisodeDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy title this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public EpisodeDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy title specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load title from a title provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_episode_detail, container, false);
+
+        // Show the dummy title as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.episode_detail)).setText(mItem.title);
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/ilkkalaukkanen/haavi/dummy/DummyContent.java
@@ -0,0 +1,65 @@
+package com.ilkkalaukkanen.haavi.dummy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample title for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static {
+        // Add 3 sample items.
+      try {
+        addItem(new DummyItem("1", "Item 1", "First item description", new URL("http://www.example.com/1.mp3")));
+        addItem(new DummyItem("2", "Item 2", "Second item description", new URL("http://www.example.com/2.mp3")));
+        addItem(new DummyItem("3", "Item 3", "Third item description", new URL("http://www.example.com/3.mp3")));
+      } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of title.
+     */
+    public static class DummyItem {
+      public final  String id;
+      public final  String title;
+      private final String description;
+      private final URL    url;
+
+      public DummyItem(String id, String content, String description, URL url) {
+        this.id = id;
+        this.title = content;
+        this.description = description;
+        this.url = url;
+      }
+
+      @Override
+      public String toString() {
+        return title + " " + description + " " + url.toString();
+      }
+    }
+}

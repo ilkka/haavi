a34d94d3825ea1ad8cf84e42053cf07ee8ca529a
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2014-01-12 20:51:32 +0200

    Configure our own injector module
    We're gonna inject our http client class with this since we want a
    custom factory for that.

---
+++ app/src/main/java/com/ilkkalaukkanen/haavi/AppModule.java
@@ -0,0 +1,13 @@
+package com.ilkkalaukkanen.haavi;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by ilau on 2014-01-12.
+ */
+public class AppModule extends AbstractModule {
+  @Override
+  protected void configure() {
+    
+  }
+}

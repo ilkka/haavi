2751d342dde7442af87dda585f5e82ba9b933b06
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2014-02-15 16:43:51 +0200

    Podcast model, just stores title for now

---
+++ app/src/main/java/com/ilkkalaukkanen/haavi/model/Podcast.java
@@ -0,0 +1,13 @@
+package com.ilkkalaukkanen.haavi.model;
+
+public class Podcast {
+    private final String title;
+
+    public Podcast(final String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}

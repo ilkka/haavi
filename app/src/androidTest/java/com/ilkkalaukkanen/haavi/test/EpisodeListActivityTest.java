281d42d55139b764c1037f32270cd1081e8c9114
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2014-03-19 20:42:35 +0200

    Upgrade to build tools 0.9.+
    instrumentTest changed to androidTest, and dropped robolectric.

---
+++ app/src/androidTest/java/com/ilkkalaukkanen/haavi/test/EpisodeListActivityTest.java
@@ -0,0 +1,11 @@
+package com.ilkkalaukkanen.haavi.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.ilkkalaukkanen.haavi.EpisodeListActivity;
+
+public class EpisodeListActivityTest extends ActivityInstrumentationTestCase2<EpisodeListActivity> {
+
+    public EpisodeListActivityTest() {
+        super(EpisodeListActivity.class);
+    }
+}
